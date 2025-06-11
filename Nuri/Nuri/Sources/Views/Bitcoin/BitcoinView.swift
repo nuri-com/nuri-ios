779e2ae7791865aa3d6b91332126889686013419
Lukas Würzburger <lukas.w@me.com>
2025-06-11 17:00:01 +0200

    11: Bitcoin screen (#24)
    * Create bitcoin view
    
    * Use bitcoin view

---
+++ Nuri/Nuri/Sources/Views/Bitcoin/BitcoinView.swift
@@ -0,0 +1,43 @@
+import SwiftUI
+
+struct BitcoinView: View {
+
+    var body: some View {
+        VStack {
+            HStack {
+                Image("card-icon")
+                Spacer()
+            }
+            Spacer()
+            HStack(spacing: 4) {
+                Text("₿")
+                HStack(spacing: 0) {
+                    Text("0.0000")
+                        .foregroundStyle(Color.secondary)
+                    Text("1337 BTC")
+                }
+            }
+            .font(.title)
+            .fontWeight(.bold)
+            Text("~$11.23 USD")
+                .font(.footnote)
+                .foregroundStyle(Color.secondary)
+            HStack {
+                Button("Receive") {
+                }
+                .buttonStyle(ProminentBlackButtonStyle())
+                Button("Send") {
+                }
+                .buttonStyle(ProminentButtonStyle())
+            }
+            Spacer()
+            Button("Transactions") {}
+        }
+        .padding(32)
+        .background(NuriAsset.background.swiftUIColor)
+    }
+}
+
+#Preview {
+    BitcoinView()
+}
