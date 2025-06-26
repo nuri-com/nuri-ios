fd66c4750e90a790ec8ef12309a1484bf3aef74a
Emin Mahrt <emin@emin.de>
2025-06-26 10:00:17 +0200

    fix: target product dependency syntax

--- Nuri/Project.swift
+++ Nuri/Project.swift
@@ -46,7 +46,7 @@ let project = Project(
             dependencies: [
                 .project(target: "Authentication", path: "../Authentication"),
                 .external(name: "CodeScanner"),
-                .external(name: "IdensicMobileSDK-iOS"),
+                .product(name: "IdensicMobileSDK", package: "IdensicMobileSDK"),
             ],
             settings: .settings(
                 base: .init()
