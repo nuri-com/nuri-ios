9b28b269d1d13c15b8c0b24d1180872fac458a0f
Emin Mahrt <emin@emin.de>
2025-06-26 10:41:44 +0200

    fix: use product name IdensicMobileSDK as external dependency

--- Nuri/Project.swift
+++ Nuri/Project.swift
@@ -46,7 +46,7 @@ let project = Project(
             dependencies: [
                 .project(target: "Authentication", path: "../Authentication"),
                 .external(name: "CodeScanner"),
-                .external(name: "idensicmobilesdk-ios"),
+                .external(name: "IdensicMobileSDK"),
             ],
             settings: .settings(
                 base: .init()
