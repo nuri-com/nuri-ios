9d2d22daf5c41736152f721464372308882518ea
Emin Mahrt <emin@emin.de>
2025-06-28 17:21:50 +0200

    style: reduce top spacing above receive/send buttons to 12pt

--- Nuri/Nuri/Sources/Views/Bitcoin/BitcoinView.swift
+++ Nuri/Nuri/Sources/Views/Bitcoin/BitcoinView.swift
@@ -35,7 +35,8 @@ struct BitcoinView: View {
                                 navigation.isSendViewPresented = true
                             }
                         }
-                        .padding(.vertical, 24)
+                        .padding(.top, 12)
+                        .padding(.bottom, 24)
                     }
                     Spacer()
                     Button(action: {
